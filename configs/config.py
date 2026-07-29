d86477303bc6510c56ce9a7eadafd5b4201e0dff
Abhinav Anand <a8hinav.anand1508@gmail.com>
2026-07-29 22:45:17 +0530

    runtime config for the simulator

---
+++ configs/config.py
@@ -0,0 +1,23 @@
+"""
+    Runtime configuration for the simulator
+"""
+
+# ======================================
+# USER SIMULATION
+# ======================================
+
+INITIAL_USER_POOL = 1000
+RETURNING_USER_PROBABILITY = 0.80
+
+# ======================================
+# FEATURE FLAG
+# ======================================
+
+ENABLE_BAD_DATA = True
+ENABLE_LATE_EVENTS = True
+
+# ======================================
+# EVENT GENERATION
+# ======================================
+
+EVENTS_PER_SECOND = 2
