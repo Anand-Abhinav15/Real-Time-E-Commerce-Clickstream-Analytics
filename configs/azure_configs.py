3a9126340decd04bc4b817a2a51ad093cf623dd6
Abhinav Anand <a8hinav.anand1508@gmail.com>
2026-08-03 17:10:54 +0530

    azure credentials stored in config

---
+++ configs/azure_configs.py
@@ -0,0 +1,16 @@
+RESOURCE_GROUP = "rg-clickstream-dev"
+
+STORAGE_ACCOUNT = "stclickstreamaa01"
+
+EVENTHUB_NAMESPACE = ""
+
+EVENTHUB_NAME = ""
+
+BRONZE_CONTAINER = "bronze"
+
+SILVER_CONTAINER = "silver"
+
+GOLD_CONTAINER = "gold"
+
+CHECKPOINT_CONTAINER = "checkpoints"
+
