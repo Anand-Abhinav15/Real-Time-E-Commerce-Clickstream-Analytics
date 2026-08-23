f926a9fc86592ee25030203d91784ee750a558da
Abhinav Anand <a8hinav.anand1508@gmail.com>
2026-08-23 20:58:32 +0530

    created gold streaming job and setup up paths

---
+++ streaming/gold_streaming_job.py
@@ -0,0 +1,43 @@
+import os
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, count, sum, when, window
+
+#Environment
+
+STORAGE_ACCOUNT = os.getenv("STORAGE_ACCOUNT")
+STORAGE_KEY = os.getenv("AZURE_STORAGE_KEY")
+
+if not STORAGE_ACCOUNT:
+    raise RuntimeError("STORAGE_ACCOUNT is not set.")
+
+if not STORAGE_KEY:
+    raise RuntimeError("AZURE_STORAGE_KEY is not set.")
+
+
+#Paths
+
+SILVER_PATH = (
+    f"abfss://silver@"
+    f"{STORAGE_ACCOUNT}.dfs.core.windows.net/"
+    f"clickstream"
+)
+
+GOLD_PRODUCT_PATH = (
+    f"abfss://gold@"
+    f"{STORAGE_ACCOUNT}.dfs.core.windows.net/"
+    f"product_trends"
+)
+
+GOLD_PRODUCT_CHECKPOINT = (
+    f"abfss://checkpoints@"
+    f"{STORAGE_ACCOUNT}.dfs.core.windows.net/"
+    f"gold_product_trends"
+)
+
+
+#Spark Session
+
+
+
+
